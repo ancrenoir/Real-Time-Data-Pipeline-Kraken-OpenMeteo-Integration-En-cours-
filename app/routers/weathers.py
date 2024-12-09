52fc08cb1e6e754a97f3352108dbff1b34caf9d9
unknown <georgesanogbre16@gmail.com>
2024-12-09 06:10:29 +0000

    Ajout de la root daily pour les variables de données jounalière

--- app/routers/weathers.py
+++ app/routers/weathers.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, HTTPException
-from app.services.weather_service import get_weather_data
+from app.services.weather_service import get_weather_data_hourly, get_weather_data_daily
 
 router = APIRouter()
 
-@router.get("/{latitude}/{longitude}")
+@router.get("/hours/{latitude}/{longitude}")
 async def weather_data(latitude: float, longitude: float):
-    data = get_weather_data(latitude, longitude)
+    data = get_weather_data_hourly(latitude, longitude)
     return data
     #if 'error' in data:
         #raise HTTPException(status_code=404, detail=data['error'])
+@router.get("/daily/{latitude}/{longitude}")
+async def weather_daily_data(latitude: float, longitude: float):
+    data= get_weather_data_daily(latitude, longitude)
+    return data
